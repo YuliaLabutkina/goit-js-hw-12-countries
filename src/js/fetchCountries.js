3d717e8a43b6da0553f6d08b6a28b6cf330a4248
Labutkina Yulia <labutkinayuliaa@gmail.com>
2020-10-07 01:04:10 +0300

    added goit-js-hw-12-countries

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -11,7 +11,7 @@ const fetchCountries = (searchQuery) => {
         } 
         return response.json()
     }).catch( err => {
-        new error(err);
+        new error(`An error has occurred! ${err}`);
     });
 }
 
